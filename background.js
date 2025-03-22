35a5acc0ba8f4afd0ae4a4b4906abb959bc3a455
JonaNetala <jonathan.netala@gmail.com>
2025-03-22 02:36:07 -0400

    Final version of all files for this assignment

---
+++ background.js
@@ -0,0 +1,3 @@
+// No background behavior is needed for this version
+// This file is here to meet the manifest structure
+// and to provide a place to add background behavior later in case it's necessary
